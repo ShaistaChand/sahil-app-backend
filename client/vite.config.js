c2fb0c515075d668d6a78bcca9ed7fe3b4824f0f
shaista chand <shaisthachand06@gmail.com>
2026-07-03 02:13:43 +0400

    fix: add netlify redirects

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/', //  THE FINAL TOUCH: Changes from './' to '/' so Render maps JavaScript scripts correctly with zero MIME type conflicts!
+  base: './', //  THE FINAL TOUCH: Changes from './' to '/' so Render maps JavaScript scripts correctly with zero MIME type conflicts!
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
